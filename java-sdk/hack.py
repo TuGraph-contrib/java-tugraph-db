d2eb0c75b17f61c91d98354eb945a5a6b7c4a18a
yangyang233333 <hfutqyy@163.com>
2023-08-29 16:45:09 +0000

    fix compiling error

--- java-sdk/hack.py
+++ java-sdk/hack.py
@@ -40,5 +40,23 @@ def fileProcessor(filePath):
                 f.truncate()
                 f.writelines(lines)
 
+# 手动修改jni_com_alibaba_tugraphjavasdk_internal_UserInfoFunc_cxx_0xc8f86253.cc文件
+def fileProcessor2(filePath):
+    TARGET_FILE = "jni_com_alibaba_tugraphjavasdk_internal_UserInfoFunc_cxx"
+    for file in os.listdir(filePath):
+        if TARGET_FILE in file:
+            target_file_path = os.path.join(filePath, file)
+            print(target_file_path)
+            with open(target_file_path,'r+') as f:
+                lines = f.readlines()
+                for i, line in enumerate(lines):
+                    if "::lgraph_api_user_info_set_roles(reinterpret_cast<lgraph_api_user_info_t*>(arg0), reinterpret_cast<char**>(arg1), arg2);" in line:
+                        lines[i] = "::lgraph_api_user_info_set_roles(reinterpret_cast<lgraph_api_user_info_t*>(arg0), reinterpret_cast<const char**>(arg1), arg2);"
+                        break
+                f.seek(0)
+                f.truncate()
+                f.writelines(lines)
+
 if __name__ == "__main__":
     fileProcessor("./target/generated-sources/annotations")
+    fileProcessor2("./target/generated-sources/annotations")
